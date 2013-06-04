b0fa5db01890777c3bebdba86feafe8237447b34
Erik Castricum <erikcas1972@gmail.com>
2013-06-05 00:31:05 +0200

    Revert "Backport bluetooth drivers from jb_mr1_chocolate onto .35 kernel"
    This reverts commit 224f51b0d9d2add9f42d3354d3e84f2565c394a0.

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1784,11 +1784,6 @@ static inline bool skb_has_frags(const struct sk_buff *skb)
 	return skb_shinfo(skb)->frag_list != NULL;
 }
 
-static inline bool skb_has_frag_list(const struct sk_buff *skb)
-{
-         return skb_shinfo(skb)->frag_list != NULL;
-}
-
 static inline void skb_frag_list_init(struct sk_buff *skb)
 {
 	skb_shinfo(skb)->frag_list = NULL;
