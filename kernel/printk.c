4cf1c65c088404f6f0b10823a661d486d55c5db2
Alin <alin.p32@gmail.com>
2014-01-26 22:11:14 +0200

    kernel: Disable printk

--- kernel/printk.c
+++ kernel/printk.c
@@ -698,6 +698,7 @@ static int have_callable_console(void)
 
 asmlinkage int printk(const char *fmt, ...)
 {
+#if 0
 	va_list args;
 	int r;
 
@@ -714,6 +715,8 @@ asmlinkage int printk(const char *fmt, ...)
 	va_end(args);
 
 	return r;
+#endif
+	return 0;
 }
 
 /* cpu currently holding logbuf_lock */
@@ -787,6 +790,7 @@ static inline void printk_delay(void)
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
+#if 0
 	int printed_len = 0;
 	int current_log_level = default_message_loglevel;
 	unsigned long flags;
@@ -918,6 +922,8 @@ out_restore_irqs:
 
 	preempt_enable();
 	return printed_len;
+#endif
+	return 0;
 }
 EXPORT_SYMBOL(printk);
 EXPORT_SYMBOL(vprintk);
